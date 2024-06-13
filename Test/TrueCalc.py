d8d2cf62b784f32d9096c04659635f0b50ad1f93
X I P <114109407+Xreatek@users.noreply.github.com>
2024-06-13 08:54:14 +0200

    Delete directory

--- Test/TrueCalc.py
+++
@@ -1,7 +0,0 @@
-WKey = True
-DKey = False
-AKey = False
-SKey = True
-
-mk = WKey+DKey+AKey+SKey
-print(mk)
--- Test/testdeque.py
+++
@@ -1,9 +0,0 @@
-import collections
-
-box = collections.deque(maxlen=1) # I <3 deque
-
-box.append("a")
-box.append("b")
-box.append("c")
-
-print(box.pop())
